9c1eb3eb98afd879664a72a3b20b473eae352ec1
Hazard <lizc07@vip.qq.com>
2018-11-25 22:12:24 +0800

    Dictionaries

---
+++ PV_INI6.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+
+# Intro to Python dictionary
+# Problem
+# Given: A string s of length at most 10000 letters.
+# Return: The number of occurrences of each word in s, where words are separated by spaces. 
+# Words are case-sensitive, and the lines in the output can be in any order.
+#
+# Sample Dataset
+# We tried list and we tried dicts also we tried Zen
+# Sample Output
+# and 1
+# We 1
+# tried 3
+# dicts 1
+# list 1
+# we 2
+# also 1
+# Zen 1
+
+s='We tried list and we tried dicts also we tried Zen'
+d={}
+for i in s.split(' '):
+  if i in d: # 'dict' object has no attribute 'has_key' in python3
+    d[i] += 1
+  else:
+    d[i] = 1
+
+for k, v in d.items():
+  print(k, v)
+  
+'''
+>>> s='We tried list and we tried dicts also we tried Zen'
+>>> d={}
+>>> for i in s.split(' '):
+...   if i in d: # 'dict' object has no attribute 'has_key' in python3
+...     d[i] += 1
+...   else:
+...     d[i] = 1
+... 
+>>> for k, v in d.items():
+...   print(k, v)
+... 
+We 1
+tried 3
+list 1
+and 1
+we 2
+dicts 1
+also 1
+Zen 1
+'''
