2f8404bc9b9700d5fd4a7b515fd32379d9b7e2ec
Hazard <lizc07@vip.qq.com>
2018-11-25 21:20:32 +0800

    Variables and Some Arithmetic

---
+++ PV_INI2.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+# Variables and Some Arithmetic
+
+# Problem
+# Given: Two positive integers a and b, each less than 1000.
+# Return: The integer corresponding to the square of the hypotenuse of the right triangle whose legs have lengths a and b.
+
+# Sample Dataset
+# 3 5
+# Sample Output
+# 34
+
+a=3
+b=5
+c=a**2+b**2
+print(c)
+
+'''
+>>> a=3
+>>> b=5
+>>> c=a**2 + b**2
+>>> print(c)
+34
+'''
