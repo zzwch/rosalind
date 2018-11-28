016029866fc943b85bbb9fecba0d7b4bbdf5f963
Hazard <lizc07@vip.qq.com>
2018-11-28 22:37:55 +0800

    Complementing a Strand of DNA
    using str methods maketrans and translate

--- BS_REVC.py
+++ BS_REVC.py
@@ -20,13 +20,25 @@
 # Solution 1
 s='AAAACCCGGT'
 d={'A':'T', 'T':'A', 'G':'C', 'C':'G'}
-for i in reversed(s):
+for i in reversed(s): 
     print(d[i], end='')
 
+print('\n')
+
 # Solution 2
+s='AAAACCCGGT'
+d={'A':'T', 'T':'A', 'G':'C', 'C':'G'}
+sc=[]
+for i in reversed(s): 
+    sc.append(d[i])
+
+print(''.join(sc))
+
+# Solution 3
 with open('rosalind_revc.txt', 'r') as f:
     s=f.readline().strip()
-    print(s.replace('A','t').replace('T','a').replace('G','c').replace('C','g').upper()[::-1])
+    print(s.replace('A','t').replace('T','a').replace('G','c').replace('C','g').upper()[::-1]) # Option 1
+    # print(s.translate(str.maketrans('ATGC','TACG'))[::-1]) # Option 2
 
 #########################################################
 # you may add your solutions below
