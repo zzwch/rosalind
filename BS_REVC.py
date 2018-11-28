9e6f7b09b4cefb4f559f17d3fafb687771b58bf4
Hazard <lizc07@vip.qq.com>
2018-11-28 22:27:05 +0800

    Complementing a Strand of DNA

---
+++ BS_REVC.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+# Complementing a Strand of DNA
+
+# Problem
+# In DNA strings, symbols 'A' and 'T' are complements of each other, as are 'C' and 'G'.
+# The reverse complement of a DNA string s is the string sc formed by reversing the symbols of s, 
+# then taking the complement of each symbol (e.g., the reverse complement of "GTCA" is "TGAC").
+
+# Given: A DNA string s of length at most 1000 bp.
+# Return: The reverse complement sc of s.
+# Sample Dataset
+# AAAACCCGGT
+# Sample Output
+# ACCGGGTTTT
+
+##########################
+# Solution from Hazard
+##########################
+# Solution 1
+s='AAAACCCGGT'
+d={'A':'T', 'T':'A', 'G':'C', 'C':'G'}
+for i in reversed(s):
+    print(d[i], end='')
+
+# Solution 2
+with open('rosalind_revc.txt', 'r') as f:
+    s=f.readline().strip()
+    print(s.replace('A','t').replace('T','a').replace('G','c').replace('C','g').upper()[::-1])
+
+#########################################################
+# you may add your solutions below
+# Don't hesitate to send pull request for your new solution 
