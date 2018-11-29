cacc52a274abe3091bd2843cbbb244bce65533e6
Hazard <lizc07@vip.qq.com>
2018-11-30 00:35:12 +0800

    Update BS_PROT.py

--- BS_PROT.py
+++ BS_PROT.py
@@ -19,24 +19,24 @@
 # Solution from Hazard
 ##########################
 # Solution 1
-codon_table='\
-UUU F      CUU L      AUU I      GUU V \
-UUC F      CUC L      AUC I      GUC V \
-UUA L      CUA L      AUA I      GUA V \
-UUG L      CUG L      AUG M      GUG V \
-UCU S      CCU P      ACU T      GCU A \
-UCC S      CCC P      ACC T      GCC A \
-UCA S      CCA P      ACA T      GCA A \
-UCG S      CCG P      ACG T      GCG A \
-UAU Y      CAU H      AAU N      GAU D \
-UAC Y      CAC H      AAC N      GAC D \
-UAA Stop   CAA Q      AAA K      GAA E \
-UAG Stop   CAG Q      AAG K      GAG E \
-UGU C      CGU R      AGU S      GGU G \
-UGC C      CGC R      AGC S      GGC G \
-UGA Stop   CGA R      AGA R      GGA G \
-UGG W      CGG R      AGG R      GGG G \
-'.split(' ')
+codon_table='''
+UUU F      CUU L      AUU I      GUU V
+UUC F      CUC L      AUC I      GUC V
+UUA L      CUA L      AUA I      GUA V
+UUG L      CUG L      AUG M      GUG V
+UCU S      CCU P      ACU T      GCU A
+UCC S      CCC P      ACC T      GCC A
+UCA S      CCA P      ACA T      GCA A
+UCG S      CCG P      ACG T      GCG A
+UAU Y      CAU H      AAU N      GAU D
+UAC Y      CAC H      AAC N      GAC D
+UAA Stop   CAA Q      AAA K      GAA E
+UAG Stop   CAG Q      AAG K      GAG E
+UGU C      CGU R      AGU S      GGU G
+UGC C      CGC R      AGC S      GGC G
+UGA Stop   CGA R      AGA R      GGA G
+UGG W      CGG R      AGG R      GGG G
+'''.replace('\n', ' ').split(' ')
 codon_table=[i for i in codon_table if i != '']
 
 codon_dict={}
@@ -50,7 +50,6 @@ with open('rosalind_prot.txt', 'r') as f:
             break
         else:
             print(aa,end="")
-
 
 #########################################################
 # you may add your solutions below
