0ba04aeb498ff5177dc083a71f7151b99b365994
verohan <39477779+verohan@users.noreply.github.com>
2018-12-04 15:31:02 +0800

    Update PV_INI6.py

--- PV_INI6.py
+++ PV_INI6.py
@@ -96,3 +96,18 @@ Zen 1
 ...
 
 
+## results from hehan @20181204
+>>> c = 'When I find myself in times of trouble Mother Mary comes to me Speaking words of wisdom let it be And in my hour of darkness she is standing right in front of me Speaking words of wisdom let it be Let it be let it be let it be let it be Whisper words of wisdom let it be And when the broken hearted people living in the world agree There will be an answer let it be For though they may be parted there is still a chance that they will see There will be an answer let it be Let it be let it be let it be let it be There will be an answer let it be Let it be let it be let it be let it be Whisper words of wisdom let it be Let it be let it be let it be let it be Whisper words of wisdom let it be And when the night is cloudy there is still a light that shines on me Shine until tomorrow let it be I wake up to the sound of music Mother Mary comes to me Speaking words of wisdom let it be Let it be let it be let it be yeah let it be There will be an answer let it be Let it be let it be let it be yeah let it be Whisper words of wisdom let it be'
+>>> c = c.split()
+>>> d = {}
+>>> for word in c:
+...     if word in d:
+...             d[word] += 1
+...     else:
+...             d [word] = 1
+...
+>>> print(d)
+>>> for key, value in d.items():
+...     print(key,value)
+...
+
